5ebcb2c7daeab6aead188ee23e4f92a6dcc053b6
Denis Taranenko <denis.taranenko@windmill.ch>
2018-08-20 15:20:02 +0300

    Fix cezar_cipher.  Make code more pythonic.

--- cezar/cezar_cipher.py
+++ cezar/cezar_cipher.py
@@ -1,68 +1,13 @@
-import config
+from string import ascii_letters
+
+l = '{}{}'.format(ascii_letters, r'1234567890 `~-=_+/*[]{}|;:/?.>,<\\\'\"')
 
 
-def cipher(input_str, key):
-    check_key(key)
-
-    result_data = []
-
-    for ch in input_str:
-        try:
-            is_title = ch.istitle()
-            index = config.eng_alphabet.index(
-                ch if not is_title else ch.lower()
-            )
-
-            new_index = index + key
-
-            if (new_index > config.len_eng_alphabet):
-                new_index = new_index - config.len_eng_alphabet
-
-            new_char = config.eng_alphabet[new_index]
-
-            if is_title and not new_char.istitle():
-                new_char = new_char.upper()
-
-            result_data.append(new_char)
-
-        except ValueError:
-            result_data.append(ch)
-
-    return ''.join(result_data)
+def cipher(msg, key):
+    assert isinstance(key, int)
+    return ''.join([l[(l.index(c) + key) % len(l)] for c in msg if c in l])
 
 
-def decipher(_str, key):
-    check_key(key)
-
-    result_data = []
-
-    for ch in _str:
-        try:
-            is_title = ch.istitle()
-            index = config.eng_alphabet.index(
-                ch if not is_title else ch.lower()
-            )
-
-            new_index = index - key
-
-            if (new_index < 0):
-                new_index = config.len_eng_alphabet + new_index
-
-            new_char = config.eng_alphabet[new_index]
-
-            if is_title and not new_char.istitle():
-                new_char = new_char.upper()
-
-            result_data.append(new_char)
-
-        except ValueError:
-            result_data.append(ch)
-
-    return ''.join(result_data)
-
-
-def check_key(key):
-    if not isinstance(key, int):
-        raise RuntimeError(
-            'Not valid key! type of key = {}'.format(str(type(key)))
-        )
+def decipher(msg, key):
+    assert isinstance(key, int)
+    return ''.join([l[(l.index(c) - key) % len(l)] for c in msg if c in l])
