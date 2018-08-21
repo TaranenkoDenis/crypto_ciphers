89106a1d2526b672b97b3e4e50467808caf7ebfa
Denis Taranenko <denis.taranenko@windmill.ch>
2018-08-21 11:19:42 +0300

    Make code more pythonic

--- gronsfeld_cipher/gronsfeld_cipher.py
+++ gronsfeld_cipher/gronsfeld_cipher.py
@@ -1,13 +1,11 @@
 from string import ascii_letters
 from itertools import cycle
 
-alph = '{}{}'.format(ascii_letters, r'1234567890 `~-=_+/*[]{}|;:/?!.>,<')
+alph = string.printable
 
 
 def cipher(msg, key):
-    keys = formatKey(key)
-    gen = createGeneratorKeys(keys)
-
+    gen = cycle(map(int, str(key)))
     return ''.join(
         [alph[((alph.index(c) + next(gen)) % len(alph))]
          for c in msg if c in alph]
@@ -15,20 +13,8 @@ def cipher(msg, key):
 
 
 def decipher(msg, key):
-    keys = formatKey(key)
-    gen = createGeneratorKeys(keys)
-
+    gen = cycle(map(int, str(key)))
     return ''.join(
         [alph[((alph.index(c) - next(gen)) % len(alph))]
          for c in msg if c in alph]
     )
-
-
-def formatKey(key):
-    assert isinstance(key, int)
-    return [int(i) for i in str(key)]
-
-
-def createGeneratorKeys(keys):
-    for k in cycle(keys):
-        yield k
