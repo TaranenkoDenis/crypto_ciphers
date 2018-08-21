bb976eb7c79b74bbb30530d53dab46aa006fca5d
Denis Taranenko <denis.taranenko@windmill.ch>
2018-08-21 10:40:05 +0300

    fix gronsfeld_cipher

--- gronsfeld_cipher/gronsfeld_cipher.py
+++ gronsfeld_cipher/gronsfeld_cipher.py
@@ -9,7 +9,7 @@ def cipher(msg, key):
     gen = createGeneratorKeys(keys)
 
     return ''.join(
-        [alph[(alph.index(c) + next(gen) % len(alph))]
+        [alph[((alph.index(c) + next(gen)) % len(alph))]
          for c in msg if c in alph]
     )
 
